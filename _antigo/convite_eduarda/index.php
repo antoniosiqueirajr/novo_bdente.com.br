4162dad64fd6cac1af445a3807f51ee7a51b881d
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2024-03-02 13:18:56 -0300

    AAA
    AAA

---
+++ _antigo/convite_eduarda/index.php
@@ -0,0 +1,300 @@
+<!doctype html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>15 Anos - Eduarda</title>
+        <meta name='robots' content='noindex, nofollow'>
+        
+        <meta name="description" content="Confirme sua presença | Veja o local"/>
+        <meta property="og:title" content="15 Anos - Eduarda"/>
+        <meta property="og:description" content="Confirme sua presença | Veja o local"/>
+
+        <meta property="og:image" content="https://bdente.com.br/convite_eduarda/thumb.png"/>
+        <meta property="og:updated_time" content="1706098878"/>
+        <meta property="og:url" content="https://bdente.com.br/convite_eduarda/" />
+        <meta property="profile:username" content="Eduarda"/>
+        
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <style>
+            .convite{
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                min-height: 100vh;
+            }
+            .convite img{
+                max-width: 100%;
+            }
+/*            .pyro{
+                pointer-events: none;
+                position: fixed;
+                top: 0;
+                left: 0;
+                height: 100vh;
+                width: 100%;
+                overflow: hidden;
+            }
+            .pyro > .before, .pyro > .after {
+                position: absolute;
+                width: 5px;
+                height: 5px;
+                border-radius: 50%;
+                box-shadow: -120px -218.66667px #0000ff80, 248px -16.66667px #00ff8480, 190px 16.33333px #002bff80, -113px -308.66667px #ff009d80, -109px -287.66667px #ffb30080, -50px -313.66667px #ff006e80, 226px -31.66667px #ff400080, 180px -351.66667px #ff00d080, -12px -338.66667px #00f6ff80, 220px -388.66667px #99ff0080, -69px -27.66667px #ff040080, -111px -339.66667px #6200ff80, 155px -237.66667px #00ddff80, -152px -380.66667px #00ffd080, -50px -37.66667px #00ffdd80, -95px -175.66667px #a6ff0080, -88px 10.33333px #0d00ff80, 112px -309.66667px #005eff80, 69px -415.66667px #ff00a680, 168px -100.66667px #ff004c80, -244px 24.33333px #ff660080, 97px -325.66667px #ff006680, -211px -182.66667px #00ffa280, 236px -126.66667px #b700ff80, 140px -196.66667px #9000ff80, 125px -175.66667px #00bbff80, 118px -381.66667px #ff002f80, 144px -111.66667px #ffae0080, 36px -78.66667px #f600ff80, -63px -196.66667px #c800ff80, -218px -227.66667px #d4ff0080, -134px -377.66667px #ea00ff80, -36px -412.66667px #ff00d480, 209px -106.66667px #00fff280, 91px -278.66667px #000dff80, -22px -191.66667px #9dff0080, 139px -392.66667px #a6ff0080, 56px -2.66667px #0099ff80, -156px -276.66667px #ea00ff80, -163px -233.66667px #00fffb80, -238px -346.66667px #00ff7380, 62px -363.66667px #0088ff80, 244px -170.66667px #0062ff80, 224px -142.66667px #b300ff80, 141px -208.66667px #9000ff80, 211px -285.66667px #ff660080, 181px -128.66667px #1e00ff80, 90px -123.66667px #c800ff80, 189px 70.33333px #00ffc880, -18px -383.66667px #00ff3380, 100px -6.66667px #ff008cf0;
+                -moz-animation: 1s bang ease-out infinite backwards, 1s gravity ease-in infinite backwards, 5s position linear infinite backwards;
+                -webkit-animation: 1s bang ease-out infinite backwards, 1s gravity ease-in infinite backwards, 5s position linear infinite backwards;
+                -o-animation: 1s bang ease-out infinite backwards, 1s gravity ease-in infinite backwards, 5s position linear infinite backwards;
+                -ms-animation: 1s bang ease-out infinite backwards, 1s gravity ease-in infinite backwards, 5s position linear infinite backwards;
+                animation: 1s bang ease-out infinite backwards, 1s gravity ease-in infinite backwards, 5s position linear infinite backwards;
+            }
+
+            .pyro > .after {
+                -moz-animation-delay: 1.25s, 1.25s, 1.25s;
+                -webkit-animation-delay: 1.25s, 1.25s, 1.25s;
+                -o-animation-delay: 1.25s, 1.25s, 1.25s;
+                -ms-animation-delay: 1.25s, 1.25s, 1.25s;
+                animation-delay: 1.25s, 1.25s, 1.25s;
+                -moz-animation-duration: 1.25s, 1.25s, 6.25s;
+                -webkit-animation-duration: 1.25s, 1.25s, 6.25s;
+                -o-animation-duration: 1.25s, 1.25s, 6.25s;
+                -ms-animation-duration: 1.25s, 1.25s, 6.25s;
+                animation-duration: 1.25s, 1.25s, 6.25s;
+            }
+
+            @-webkit-keyframes bang {
+                from {
+                    box-shadow: 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white;
+                }
+            }
+            @-moz-keyframes bang {
+                from {
+                    box-shadow: 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white;
+                }
+            }
+            @-o-keyframes bang {
+                from {
+                    box-shadow: 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white;
+                }
+            }
+            @-ms-keyframes bang {
+                from {
+                    box-shadow: 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white;
+                }
+            }
+            @keyframes bang {
+                from {
+                    box-shadow: 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white, 0 0 white;
+                }
+            }
+            @-webkit-keyframes gravity {
+                to {
+                    transform: translateY(200px);
+                    -moz-transform: translateY(200px);
+                    -webkit-transform: translateY(200px);
+                    -o-transform: translateY(200px);
+                    -ms-transform: translateY(200px);
+                    opacity: 0;
+                }
+            }
+            @-moz-keyframes gravity {
+                to {
+                    transform: translateY(200px);
+                    -moz-transform: translateY(200px);
+                    -webkit-transform: translateY(200px);
+                    -o-transform: translateY(200px);
+                    -ms-transform: translateY(200px);
+                    opacity: 0;
+                }
+            }
+            @-o-keyframes gravity {
+                to {
+                    transform: translateY(200px);
+                    -moz-transform: translateY(200px);
+                    -webkit-transform: translateY(200px);
+                    -o-transform: translateY(200px);
+                    -ms-transform: translateY(200px);
+                    opacity: 0;
+                }
+            }
+            @-ms-keyframes gravity {
+                to {
+                    transform: translateY(200px);
+                    -moz-transform: translateY(200px);
+                    -webkit-transform: translateY(200px);
+                    -o-transform: translateY(200px);
+                    -ms-transform: translateY(200px);
+                    opacity: 0;
+                }
+            }
+            @keyframes gravity {
+                to {
+                    transform: translateY(200px);
+                    -moz-transform: translateY(200px);
+                    -webkit-transform: translateY(200px);
+                    -o-transform: translateY(200px);
+                    -ms-transform: translateY(200px);
+                    opacity: 0;
+                }
+            }
+            @-webkit-keyframes position {
+                0%, 19.9% {
+                    margin-top: 10%;
+                    margin-left: 40%;
+                }
+
+                20%, 39.9% {
+                    margin-top: 40%;
+                    margin-left: 30%;
+                }
+
+                40%, 59.9% {
+                    margin-top: 20%;
+                    margin-left: 70%;
+                }
+
+                60%, 79.9% {
+                    margin-top: 30%;
+                    margin-left: 20%;
+                }
+
+                80%, 99.9% {
+                    margin-top: 30%;
+                    margin-left: 80%;
+                }
+            }
+            @-moz-keyframes position {
+                0%, 19.9% {
+                    margin-top: 10%;
+                    margin-left: 40%;
+                }
+
+                20%, 39.9% {
+                    margin-top: 40%;
+                    margin-left: 30%;
+                }
+
+                40%, 59.9% {
+                    margin-top: 20%;
+                    margin-left: 70%;
+                }
+
+                60%, 79.9% {
+                    margin-top: 30%;
+                    margin-left: 20%;
+                }
+
+                80%, 99.9% {
+                    margin-top: 30%;
+                    margin-left: 80%;
+                }
+            }
+            @-o-keyframes position {
+                0%, 19.9% {
+                    margin-top: 10%;
+                    margin-left: 40%;
+                }
+
+                20%, 39.9% {
+                    margin-top: 40%;
+                    margin-left: 30%;
+                }
+
+                40%, 59.9% {
+                    margin-top: 20%;
+                    margin-left: 70%;
+                }
+
+                60%, 79.9% {
+                    margin-top: 30%;
+                    margin-left: 20%;
+                }
+
+                80%, 99.9% {
+                    margin-top: 30%;
+                    margin-left: 80%;
+                }
+            }
+            @-ms-keyframes position {
+                0%, 19.9% {
+                    margin-top: 10%;
+                    margin-left: 40%;
+                }
+
+                20%, 39.9% {
+                    margin-top: 40%;
+                    margin-left: 30%;
+                }
+
+                40%, 59.9% {
+                    margin-top: 20%;
+                    margin-left: 70%;
+                }
+
+                60%, 79.9% {
+                    margin-top: 30%;
+                    margin-left: 20%;
+                }
+
+                80%, 99.9% {
+                    margin-top: 30%;
+                    margin-left: 80%;
+                }
+            }
+            @keyframes position {
+                0%, 19.9% {
+                    margin-top: 10%;
+                    margin-left: 40%;
+                }
+
+                20%, 39.9% {
+                    margin-top: 40%;
+                    margin-left: 30%;
+                }
+
+                40%, 59.9% {
+                    margin-top: 20%;
+                    margin-left: 70%;
+                }
+
+                60%, 79.9% {
+                    margin-top: 30%;
+                    margin-left: 20%;
+                }
+
+                80%, 99.9% {
+                    margin-top: 30%;
+                    margin-left: 80%;
+                }
+            }*/
+
+        </style>
+   </head>
+   <body>
+        <audio id="musica">
+            <source src="musica.mp3" type="audio/mpeg">
+        </audio>
+        <div class="convite">
+            <img src="convite eduarda_2201_3.jpeg" usemap="#image-map">
+        </div>
+        <map name="image-map">
+            <area target="_blank" alt="Confirmar" title="Confirmar" href="https://wa.me/5541984593129?text=Gostaria%20de%20confirmar%20minha%20presença%20no%20Aniversário%20da%20Eduarda" coords="338,938,62" shape="circle">
+            <area target="_blank" alt="Endereço" title="Endereço" href="https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIPCAEQLhgNGK8BGMcBGIAE0gEINjY4M2owajeoAgCwAgA&um=1&ie=UTF-8&fb=1&gl=br&sa=X&geocode=KRvoXX1c49yUMTc9Ty-kYoSp&daddr=Av.+N.+Sra.+Aparecida,+1016+-+Semin%C3%A1rio,+Curitiba+-+PR,+80310-100" coords="570,937,61" shape="circle">
+            <area target="_blank" alt="Confirmar" title="Confirmar" href="https://wa.me/5541984593129?text=Gostaria%20de%20confirmar%20minha%20presença%20no%20Aniversário%20da%20Eduarda" coords="247,1005,425,1063" shape="rect">
+            <area target="_blank" alt="Endereço" title="Endereço" href="https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIPCAEQLhgNGK8BGMcBGIAE0gEINjY4M2owajeoAgCwAgA&um=1&ie=UTF-8&fb=1&gl=br&sa=X&geocode=KRvoXX1c49yUMTc9Ty-kYoSp&daddr=Av.+N.+Sra.+Aparecida,+1016+-+Semin%C3%A1rio,+Curitiba+-+PR,+80310-100" coords="495,1008,649,1061" shape="rect">
+        </map>
+<!--        <div class="pyro">
+            <div class="before"></div>
+            <div class="after"></div>
+        </div>-->
+        <script src="imageMapResizer.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('map').imageMapResize();
+            });
+            $('body').on('click',function(){
+                document.getElementById('musica').play();
+            });
+        </script>
+   </body>
+</html>
--- novo/blog.php
+++
@@ -1,3 +0,0 @@
-<?php
-$pagina = 'blog';
-require './template/template.php';
